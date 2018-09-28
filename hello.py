1b7da93315a45b86be2e1d86a3cf3dc126b80975
k-manisha <kashyapmanisha29@gmail.com>
2018-09-29 00:01:28 +0530

    Hello to Python

---
+++ hello.py
@@ -0,0 +1,33 @@
+#comment single line using #
+#printing string
+print("Hello Coders")
+print("Printing numbers")
+print(1,2,3,4,5)
+print(1)
+#checking type
+print(type(1))
+print(type("hello"))
+print(type(4.6))
+
+""" multi 
+line 
+comments
+"""
+
+# Taking input
+a= input("input a character")
+print(a)
+
+b=input("input a number")
+print(b)
+
+"""a and b  are variables and their is defined by the type of value stored in them"""
+# No pre declaration required
+# checking type of variable
+print(type(a))
+print(type(b))
+
+#Both are string as used input is always taken as string
+# Type cast the them to required type
+b=int(b)
+print(b,type(b))
